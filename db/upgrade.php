7141cfd18a4ae4a4998a9f9b9ed9d465b14f37bd
Tristan Mackay <t.mackay@murdoch.edu.au>
2017-10-05 16:35:32 +0800

    fixing table stucture

--- db/upgrade.php
+++ db/upgrade.php
@@ -2,17 +2,15 @@
 function xmldb_block_heatmap_upgrade($oldversion) {
     global $CFG, $DB;
     $dbman = $DB->get_manager();
-    if ($oldversion < 2017100500) {
+    if ($oldversion < 2017100600) {
         // Define table heatmap to be created.
         $table = new xmldb_table('block_heatmap');
         // Adding fields to table heatmap.
-        $table->add_field('cmid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('cmid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0');
         $table->add_field('numviews', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0');
         $table->add_field('distinctusers', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0');
         $table->add_field('courseid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0');
         $table->add_field('status', XMLDB_TYPE_INTEGER, '4', null, XMLDB_NOTNULL, null, '0');
-        // Adding keys to table heatmap.
-        $table->add_key('primary', XMLDB_KEY_PRIMARY, array('cmid'));
         // Adding indexes to table heatmap.
         $table->add_index('courseid', XMLDB_INDEX_NOTUNIQUE, array('courseid'));
  
@@ -21,7 +19,7 @@ function xmldb_block_heatmap_upgrade($oldversion) {
             $dbman->create_table($table);
         }
         // heatmap savepoint reached.
-        upgrade_block_savepoint(true, 2017100500, 'heatmap');
+        upgrade_block_savepoint(true, 2017100600, 'heatmap');
 }
 return true;
 }
