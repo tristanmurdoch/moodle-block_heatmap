689b998459a8c8dbff933adce2a942b01fbc4480
tristanmurdoch <t.mackay@murdoch.edu.au>
2020-04-19 22:16:17 +0800

    only getting cache for courses with current access

--- classes/task/export_data.php
+++ classes/task/export_data.php
@@ -11,59 +11,51 @@ class export_data extends \core\task\scheduled_task {
     global $CFG, $DB;
 
 
-	  // get courses with heatmap block
-	  $sql = "SELECT c.id,c.startdate
-        FROM {context} cx
-        JOIN {block_instances} bi ON bi.parentcontextid = cx.id 
-        JOIN {course} c ON c.id = cx.instanceid 
-        WHERE bi.blockname LIKE 'heatmap'
-        GROUP BY 1";
+	    // get courses with heatmap block
+        $sql = "SELECT c.id,c.startdate
+            FROM {context} cx
+            JOIN {block_instances} bi ON bi.parentcontextid = cx.id 
+            JOIN {course} c ON c.id = cx.instanceid 
+            WHERE bi.blockname LIKE 'heatmap'
+            AND c.id in (SELECT DISTINCT courseid FROM {user_lastaccess}
+            WHERE timeaccess >= TRUNC(extract(EPOCH FROM now() - INTERVAL '24 HOURS')))
+            GROUP BY 1";
 
-	    $courses = $DB->get_records_sql($sql);
+        $courses = $DB->get_records_sql($sql);
+        $activitysince = get_config('block_heatmap', 'activitysince');
+            if ($activitysince === false) {
+                $activitysince = 'sincestart';
+            }
 
-
-    $activitysince = get_config('block_heatmap', 'activitysince');
-        if ($activitysince === false) {
-            $activitysince = 'sincestart';
-        }
-
-	  // get data using courses data
-
-    //$data = array();
-    
-    foreach($courses as $course){
-        if(isset($course->id)){
-            $timesince = ($activitysince == 'sincestart' or $activitysince == 'sincestart-date') ? 'AND timecreated >= :coursestart' : '';
+	    // get data using courses data
+        foreach($courses as $course){
+            if(isset($course->id)){
+                $timesince = ($activitysince == 'sincestart') ? 'AND timecreated >= :coursestart' : '';
+            
                 $sql = "SELECT contextinstanceid as cmid, COUNT('x') AS numviews, COUNT(DISTINCT userid) AS distinctusers, '$course->id' as courseid, '0' as status
-                          FROM {logstore_standard_log} l
-                         WHERE courseid = :courseid
-                           $timesince
-                           AND anonymous = 0
-                           AND crud = 'r'
-                           AND contextlevel = :contextmodule
-                      GROUP BY contextinstanceid";
+                        FROM {logstore_standard_log} l
+                        WHERE courseid = :courseid
+                        $timesince
+                        AND anonymous = 0
+                        AND crud = 'r'
+                        AND contextlevel = :contextmodule
+                        GROUP BY contextinstanceid";
 
-	          $params = array('courseid' => $course->id, 'contextmodule' => CONTEXT_MODULE, 'coursestart' => $course->startdate );
-            $views = $DB->get_records_sql($sql, $params);
-            //var_dump($views);
-            $records[] = $views;
-	      }
-    }
-  // flag data that needs deleted
+	            $params = array('courseid' => $course->id, 'contextmodule' => CONTEXT_MODULE, 'coursestart' => $course->startdate );
+                $views = $DB->get_records_sql($sql, $params);
 
-    $sql_update = 'UPDATE {block_heatmap} SET status = 1';
-    $DB->execute($sql_update, $parms=null);
-
-    foreach ($records as $record) {
-        foreach ($record as $rec){
-        $lastinsertid = $DB->insert_record('block_heatmap', $rec, false);
+                foreach ($views as $view) {
+                    if ($DB->get_record('block_heatmap', (array) $view)) {
+                        // TODO use $DB->update_record but need to change table to have ID.
+                        echo "record needs updating\n";
+                        $DB->delete_records('block_heatmap', (array) $view);
+                        $DB->insert_record('block_heatmap', $view, false);
+                    } else {
+                        echo "record needs inserting\n";
+                        $DB->insert_record('block_heatmap', $view, false);
+                    }
+                }
+            }
         }
     }
-
-    // delete old data
-    $params = array('status' => '1');
-    $DB->delete_records('block_heatmap',$params);
-
-    }                                                                                                                               
-} 
-
+}
